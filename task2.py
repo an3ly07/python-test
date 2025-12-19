993ead754cce9578d14ccac47ff3bd97dc4bf638
an3ly07 <yelmuratanel991@gmail.com>
2025-12-19 16:22:16 +0500

    task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,10 @@
-salaries = list(map(int, input().split()))
-print(max(salaries) - min(salaries))
+salaries = input().split()
+
+a = int(salaries[0])
+b = int(salaries[1])
+c = int(salaries[2])
+
+maximum = max(a, b, c)
+minimum = min(a, b, c)
+
+print(maximum - minimum)
