29e31b8d9f59498714f6ab217c4444eb0e7c06b4
an3ly07 <yelmuratanel991@gmail.com>
2025-12-19 16:24:54 +0500

    task9.py

--- task9.py
+++ task9.py
@@ -1,6 +1,17 @@
 ticket = input()
 
-first_sum = sum(map(int, ticket[:3]))
-second_sum = sum(map(int, ticket[3:]))
+a = int(ticket[0])
+b = int(ticket[1])
+c = int(ticket[2])
 
-print("YES" if first_sum == second_sum else "NO")
+d = int(ticket[3])
+e = int(ticket[4])
+f = int(ticket[5])
+
+first_sum = a + b + c
+second_sum = d + e + f
+
+if first_sum == second_sum:
+    print("YES")
+else:
+    print("NO")
